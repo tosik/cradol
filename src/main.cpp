0dd10df8bddcbfc11f9a165ddbcf84302d197aec
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-03-31 14:20:25 +0900

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+
+#include "basic/Main.h"
+#include "for/win/MainFunction.h"
+
+int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, LPSTR lpszArgs, int nWinMode)
+{
+	// Windows 用のアプリケーションの場合は、この関数に引数をそのまま受け渡す
+	return BlueCarrot::WindowsEntryPoint(hThisInst, hPrevInst, lpszArgs, nWinMode);
+}
+
+
+
+BlueCarrot::SceneBase * BlueCarrot::callback::CreateScene(BlueCarrot::SceneID scene_id)
+{
+	// TODO : シーンを返してね
+	return NULL;
+}
+
+BlueCarrot::SceneID BlueCarrot::callback::GetStartupSceneID()
+{
+	// TODO : スタート時のシーン ID を返してね
+	return 0;
+}
+
+BlueCarrot::utility::Size<int> BlueCarrot::callback::GetScreenSize()
+{
+	return BlueCarrot::utility::Size<int>(640, 480);
+}
